3367089500eb848dade580bfe0858f907a4bbb1a
Cal <acc@calrichards.io>
2025-11-26 13:04:22 +0000

    fix: wake from sleep notif

--- src/event_observers.rs
+++ src/event_observers.rs
@@ -1,8 +1,62 @@
 use objc2::rc::Retained;
-use objc2_foundation::{ns_string, NSNotificationCenter, NSString};
+use objc2_app_kit::NSWorkspace;
+use objc2_foundation::{NSNotificationCenter, NSString};
 
 use crate::ffi::foundation;
 use crate::menu::MenuDelegate;
+
+#[derive(Debug, Clone, Copy)]
+enum NotificationCenter {
+    Default,
+    Workspace,
+}
+
+impl NotificationCenter {
+    fn get(&self) -> Retained<NSNotificationCenter> {
+        match self {
+            Self::Default => NSNotificationCenter::defaultCenter(),
+            Self::Workspace => NSWorkspace::sharedWorkspace().notificationCenter(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct NotificationConfig {
+    name: &'static str,
+    selector: &'static str,
+    center: NotificationCenter,
+}
+
+impl NotificationConfig {
+    const fn new(name: &'static str, selector: &'static str, center: NotificationCenter) -> Self {
+        Self {
+            name,
+            selector,
+            center,
+        }
+    }
+
+    fn name(&self) -> Retained<NSString> {
+        NSString::from_str(self.name)
+    }
+
+    fn selector(&self) -> objc2::runtime::Sel {
+        match self.selector {
+            "eventStoreChanged:" => objc2::sel!(eventStoreChanged:),
+            "didWakeNotification:" => objc2::sel!(didWakeNotification:),
+            _ => unreachable!("Unknown selector"),
+        }
+    }
+
+    fn center(&self) -> Retained<NSNotificationCenter> {
+        self.center.get()
+    }
+
+    fn register(&self, delegate: &Retained<MenuDelegate>) {
+        let name = self.name();
+        foundation::add_observer(&self.center(), delegate, self.selector(), Some(&name), None);
+    }
+}
 
 #[derive(Debug, Clone, Copy)]
 pub enum SystemNotification {
@@ -11,45 +65,38 @@ pub enum SystemNotification {
 }
 
 impl SystemNotification {
-    fn name(&self) -> &NSString {
+    const fn config(&self) -> NotificationConfig {
         match self {
-            Self::EventStoreChanged => ns_string!("EKEventStoreChangedNotification"),
-            Self::WorkspaceDidWake => ns_string!("NSWorkspaceDidWakeNotification"),
+            Self::EventStoreChanged => NotificationConfig::new(
+                "EKEventStoreChangedNotification",
+                "eventStoreChanged:",
+                NotificationCenter::Default,
+            ),
+            Self::WorkspaceDidWake => NotificationConfig::new(
+                "NSWorkspaceDidWakeNotification",
+                "didWakeNotification:",
+                NotificationCenter::Workspace,
+            ),
         }
     }
 
-    fn selector(&self) -> objc2::runtime::Sel {
-        match self {
-            Self::EventStoreChanged => objc2::sel!(eventStoreChanged:),
-            Self::WorkspaceDidWake => objc2::sel!(didWakeNotification:),
-        }
-    }
-
-    fn add(&self, center: &NSNotificationCenter, delegate: &Retained<MenuDelegate>) {
-        foundation::add_observer(center, delegate, self.selector(), Some(self.name()), None);
+    fn register(&self, delegate: &Retained<MenuDelegate>) {
+        self.config().register(delegate);
     }
 }
 
 pub struct SystemNotificationObserver<'a> {
-    notification_center: Retained<NSNotificationCenter>,
     delegate: &'a Retained<MenuDelegate>,
 }
 
 impl<'a> SystemNotificationObserver<'a> {
     pub fn new(delegate: &'a Retained<MenuDelegate>) -> Self {
-        Self {
-            notification_center: NSNotificationCenter::defaultCenter(),
-            delegate,
-        }
+        Self { delegate }
     }
 
     pub fn register(self) -> Self {
-        self.add_notification(SystemNotification::EventStoreChanged);
-        self.add_notification(SystemNotification::WorkspaceDidWake);
+        SystemNotification::EventStoreChanged.register(self.delegate);
+        SystemNotification::WorkspaceDidWake.register(self.delegate);
         self
-    }
-
-    fn add_notification(&self, notification: SystemNotification) {
-        notification.add(&self.notification_center, &self.delegate);
     }
 }
